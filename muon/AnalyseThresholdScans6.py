c798f8d3675048673cb98dbabe27d664e88d4b3e
James Lord <james.lord@stfc.ac.uk>
2013-11-19 13:19:13 +0000

    Updated for Python API v2

--- muon/AnalyseThresholdScans6.py
+++ muon/AnalyseThresholdScans6.py
@@ -1,4 +1,5 @@
-from MantidFramework import *
+from mantid.kernel import *
+from mantid.api import *
 import math
  
 class AnalyseThresholdScans(PythonAlgorithm):
@@ -6,25 +7,25 @@ class AnalyseThresholdScans(PythonAlgorithm):
     def PyInit(self):
         # Declare properties
 	# first and last runs to be loaded as for PlotAsymmetryByLogValue
-	self.declareFileProperty("FirstFile","",FileAction.Load, Exts = ["nxs"],Description="First of a continuous sequence of runs measured at different threshold settings")
-	self.declareProperty("NumberOfRuns",1000000,Description="Number of runs, default continues until sequence stops. All must have threshold in Comment line")
+	self.declareProperty(FileProperty("FirstFile","",FileAction.Load, extensions = ["nxs"]),doc="First of a continuous sequence of runs measured at different threshold settings")
+	self.declareProperty("NumberOfRuns",1000000,doc="Number of runs, default continues until sequence stops. All must have threshold in Comment line")
 	# use as many files as entries in thresholds list
 	#self.declareFileProperty("LastFile","",FileAction.Load, Exts = ["nxs"])
 	# assume thresholds are set in Comment line "thresholds  123mV" and in increasing order
 	# self.declareListProperty("Thresholds",[25.0,37.0,50.0,62.0,75.0,87.0,100.0,125.0,150.0],Description="Comma separated list of the thresholds used")
 	# if TF set to 0 then assume ZF / high LF data
-	self.declareProperty("TransverseFieldGuess",20.0,Description="Hint for fit, or 0 for ZF/LF non-relaxing data, -1 for integral counts")
-	self.declareProperty("FitDataFrom",20.0,Description="Start time for fit, may need to avoid high count rate distortions. Always fits to end of histograms.")
-	self.declareProperty("FixMuonLifetime",DefaultValue=True,Description="Could allow muon lifetime to vary with uncalibrated TDC and high stats runs")
-	self.declareProperty("FixBackgroundToZero",DefaultValue=True,Description="Could fit background rate (random, light-leak) with high stats only")
-	self.declareProperty("Grouped",DefaultValue=False,Description="Group detectors before fitting")
+	self.declareProperty("TransverseFieldGuess",20.0,doc="Hint for fit, or 0 for ZF/LF non-relaxing data, -1 for integral counts")
+	self.declareProperty("FitDataFrom",20.0,doc="Start time for fit, may need to avoid high count rate distortions. Always fits to end of histograms.")
+	self.declareProperty("FixMuonLifetime",True,doc="Could allow muon lifetime to vary with uncalibrated TDC and high stats runs")
+	self.declareProperty("FixBackgroundToZero",True,doc="Could fit background rate (random, light-leak) with high stats only")
+	self.declareProperty("Grouped",False,doc="Group detectors before fitting")
 
     def category(self):
         return 'Muon'
 
     def PyExec(self):
         # Run the algorithm
-        firstfn = self.getProperty('FirstFile')
+        firstfn = self.getProperty('FirstFile').value
 	i=firstfn.rindex('.')
 	j=i-1
 	while firstfn[j-1].isdigit():
@@ -40,12 +41,12 @@ class AnalyseThresholdScans(PythonAlgorithm):
 #path2="\\\\hifi\\data\\"
 #inst="hifi000"
 #extn=".nxs"
-	bguess=self.getProperty('TransverseFieldGuess')
-	nmax=self.getProperty('NumberOfRuns')
-	t1=self.getProperty('FitDataFrom')
-	fml=self.getProperty('FixMuonLifetime')
-	fbz=self.getProperty('FixBackgroundToZero')
-	agroup=self.getProperty('Grouped')
+	bguess=self.getProperty('TransverseFieldGuess').value
+	nmax=self.getProperty('NumberOfRuns').value
+	t1=self.getProperty('FitDataFrom').value
+	fml=self.getProperty('FixMuonLifetime').value
+	fbz=self.getProperty('FixBackgroundToZero').value
+	agroup=self.getProperty('Grouped').value
 	
 # table workspace would be nice but there's no way to create it?!
 # also no way to extract Comment line from a run?
@@ -77,11 +78,11 @@ class AnalyseThresholdScans(PythonAlgorithm):
 	  #self.progress(0.25*vi/numruns)
 	  try:
 	    print "calling LoadMuonNexus with ",thisrunpath," and ",thisrunname
-	    LoadMuonNexus(filename=thisrunpath,OutputWorkspace=thisrunname,AutoGroup=agroup)
+	    thisoneX=LoadMuonNexus(filename=thisrunpath,OutputWorkspace=thisrunname,AutoGroup=agroup)
 	  except:
 	    print "no more runs to load"
 	    break
-	  thisone=mtd.getMatrixWorkspace(thisrunname)
+	  thisone=thisoneX[0]
 	  print "Comment field: [",thisone.getComment(),"]"
 	  comnt=thisone.getComment()
 	  mv=comnt.find("mV")
@@ -99,16 +100,16 @@ class AnalyseThresholdScans(PythonAlgorithm):
 	    oldvolts=999999
 	    break
 	  if(vi==0):
-	    CloneWorkspace(InputWorkspace=runnames[vi],OutputWorkspace="Summed")
-	    ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="rates")
-	    proc=mtd.getMatrixWorkspace("rates")
-	    ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="Asyms")
-	    procA=mtd.getMatrixWorkspace("Asyms")
-	    ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="BGs")
-	    procB=mtd.getMatrixWorkspace("BGs")
-	    ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="DTs")
-	    procD=mtd.getMatrixWorkspace("DTs")
-	    sumd=mtd.getMatrixWorkspace("summed")
+	    sumd=CloneWorkspace(InputWorkspace=runnames[vi],OutputWorkspace="Summed")
+	    proc=ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="rates")
+	    #proc=mtd.getMatrixWorkspace("rates")
+	    procA=ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="Asyms")
+	    #procA=mtd.getMatrixWorkspace("Asyms")
+	    procB=ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="BGs")
+	    #procB=mtd.getMatrixWorkspace("BGs")
+	    procD=ConvertToPointData(InputWorkspace=runnames[vi],OutputWorkspace="DTs")
+	    #procD=mtd.getMatrixWorkspace("DTs")
+	    #sumd=mtd.getMatrixWorkspace("summed")
 	  else:
 	    sumd += thisone
 	  vi=vi+1
@@ -130,29 +131,35 @@ class AnalyseThresholdScans(PythonAlgorithm):
 		  else:
 		    phguess=6.28
 		  nguess=sumd.dataY(i)[64]
-		  ff=mtd.createAlgorithm("Fit")
-		  ff.setPropertyValue("Function","name=UserFunction, Formula=1/(1/(n*(1+a*cos(x*f+p))*exp(-x/tau)+b)+d), n="+str(nguess)+", a=0.2, f="+str(bguess*0.01355*3.14159*2.0)+", p="+str(phguess)+", b=0.01, d=0.001, tau=2.19703")
-		  ff.setPropertyValue("InputWorkspace","summed")
-		  ff.setPropertyValue("StartX",str(t1))
-		  ff.setPropertyValue("EndX","32")
-		  ff.setPropertyValue("Output","fitres")
+		  #ff=mantid.createAlgorithm("Fit") # had mtd.createAlgorithm
+		  #ff.setPropertyValue("Function","name=UserFunction, Formula=1/(1/(n*(1+a*cos(x*f+p))*exp(-x/tau)+b)+d), n="+str(nguess)+", a=0.2, f="+str(bguess*0.01355*3.14159*2.0)+", p="+str(phguess)+", b=0.01, d=0.001, tau=2.19703")
+		  #ff.setPropertyValue("InputWorkspace","summed")
+		  #ff.setPropertyValue("StartX",str(t1))
+		  #ff.setPropertyValue("EndX","32")
+		  #ff.setPropertyValue("Output","fitres")
 		  #print "Predicting f=",str(bguess*0.01355*3.14159*2.0)," and p=",str(phguess)
-		  ff.setPropertyValue("WorkspaceIndex",str(i))
+		  #ff.setPropertyValue("WorkspaceIndex",str(i))
 		  if(fml):
 			if(fbz):
-				ff.setPropertyValue("Ties","tau=2.19703,b=0.0")
+				#ff.setPropertyValue("Ties","tau=2.19703,b=0.0")
+				ties="tau=2.19703,b=0.0"
 			else:
-				ff.setPropertyValue("Ties","tau=2.19703")
+				#ff.setPropertyValue("Ties","tau=2.19703")
+				ties="tau=2.19703"
 		  else:
 			if(fbz):
-				ff.setPropertyValue("Ties","b=0.0")
-		  ff.execute()
-		  fr=mtd.getTableWorkspace("fitres_Parameters")
-		  afitted=fr.getDouble("Value",1)
-		  bfitted=fr.getDouble("Value",2)
-		  phfitted=fr.getDouble("Value",3)
-		  taufitted=fr.getDouble("Value",4)
-		  if(fr.getDouble("Value",1)<0):
+				#ff.setPropertyValue("Ties","b=0.0")
+				ties="b=0.0"
+			else:
+				ties=""
+		  #ff.execute()
+		  ff=Fit(Function="name=UserFunction, Formula=1/(1/(n*(1+a*cos(x*f+p))*exp(-x/tau)+b)+d), n="+str(nguess)+", a=0.2, f="+str(bguess*0.01355*3.14159*2.0)+", p="+str(phguess)+", b=0.01, d=0.001, tau=2.19703",InputWorkspace="Summed",StartX=str(t1),EndX=32,Output="fitres",Ties=ties,WorkspaceIndex=str(i))
+		  fr=mtd["fitres_Parameters"]
+		  afitted=fr.cell("Value",1)
+		  bfitted=fr.cell("Value",2)
+		  phfitted=fr.cell("Value",3)
+		  taufitted=fr.cell("Value",4)
+		  if(fr.cell("Value",1)<0):
 		   phfitted=phfitted+3.14159265 # correct for phase jumping negative
 		   afitted=-afitted
 		  print "hist ",i," freq=",bfitted," ampl=",afitted," phase ",phfitted," tau=", taufitted
@@ -160,31 +167,36 @@ class AnalyseThresholdScans(PythonAlgorithm):
 			  taufitted=2.19703
 		  for vi in range(0,len(voltlist)):
 	    # now fit the individual runs' spectra
-		    nfst0=runhandles[vi].getSampleDetails().getLogData("ICPEVENT").value
-		    gfi=nfst0.rfind("GF")
-		    gfie=nfst0.rfind("RF")
-		    nframes=int(nfst0[gfi+2:gfie])
+		    #nfst0=runhandles[vi].getSampleDetails().getLogData("ICPEVENT").value
+		    #gfi=nfst0.rfind("GF")
+		    #gfie=nfst0.rfind("RF")
+		    #nframes=int(nfst0[gfi+2:gfie])
+		    nframes=runhandles[vi].getSampleDetails().getLogData("goodfrm").value
 		    tres=runhandles[vi].readX(i)[2]-runhandles[vi].readX(i)[1]  # microseconds
-		    ff=mtd.createAlgorithm("Fit")
+		    #ff=mtd.createAlgorithm("Fit")
 		    nguess=runhandles[vi].dataY(i)[64]
-		    ff.setPropertyValue("Function","name=UserFunction, Formula=1/(1/(n*(1+a*cos(x*"+str(bfitted)+"+"+str(phfitted)+"))*exp(-x/"+str(taufitted)+")+b)+d), n="+str(nguess)+", a=0.2, b=0.01, d=0.001")
-		    ff.setPropertyValue("InputWorkspace",runnames[vi])
-		    ff.setPropertyValue("WorkspaceIndex",str(i))
-		    ff.setPropertyValue("StartX",str(t1))
-		    ff.setPropertyValue("EndX","32")
-		    ff.setPropertyValue("Output","fitres")
+		    #ff.setPropertyValue("Function","name=UserFunction, Formula=1/(1/(n*(1+a*cos(x*"+str(bfitted)+"+"+str(phfitted)+"))*exp(-x/"+str(taufitted)+")+b)+d), n="+str(nguess)+", a=0.2, b=0.01, d=0.001")
+		    #ff.setPropertyValue("InputWorkspace",runnames[vi])
+		    #ff.setPropertyValue("WorkspaceIndex",str(i))
+		    #ff.setPropertyValue("StartX",str(t1))
+		    #ff.setPropertyValue("EndX","32")
+		    #ff.setPropertyValue("Output","fitres")
 		    if(fbz):
-			ff.setPropertyValue("Ties","b=0.0")
-		    ff.execute()
-		    fr=mtd.getTableWorkspace("fitres_Parameters")
-		    Nfitted=fr.getDouble("Value",0)
-		    NfitE=fr.getDouble("Error",0)
-		    Afitted=fr.getDouble("Value",1)
-		    AfitE=fr.getDouble("Error",1)
-		    Bfitted=fr.getDouble("Value",2)
-		    BfitE=fr.getDouble("Error",2)
-		    Dfitted=fr.getDouble("Value",3)
-		    DfitE=fr.getDouble("Error",3)
+			#ff.setPropertyValue("Ties","b=0.0")
+			ties="b=0.0"
+		    else:
+			    ties=""
+		    #ff.execute()
+		    ff=Fit(Function="name=UserFunction, Formula=1/(1/(n*(1+a*cos(x*"+str(bfitted)+"+"+str(phfitted)+"))*exp(-x/"+str(taufitted)+")+b)+d), n="+str(nguess)+", a=0.2, b=0.01, d=0.001",InputWorkspace=runnames[vi],WorkspaceIndex=str(i),StartX=str(t1),EndX=32,Output="fitres",Ties=ties)
+		    fr=mtd["fitres_Parameters"]
+		    Nfitted=fr.cell("Value",0)
+		    NfitE=fr.cell("Error",0)
+		    Afitted=fr.cell("Value",1)
+		    AfitE=fr.cell("Error",1)
+		    Bfitted=fr.cell("Value",2)
+		    BfitE=fr.cell("Error",2)
+		    Dfitted=fr.cell("Value",3)
+		    DfitE=fr.cell("Error",3)
 	    #print "fitted individual run ",vi," to give asym=",Afitted," backgd=",Bfitted," and deadtime=",Dfitted
 		    proc.dataX(i)[vi]=voltlist[vi]
 		    proc.dataY(i)[vi]=Nfitted/nframes/tres # counts/us/frame
@@ -202,35 +214,42 @@ class AnalyseThresholdScans(PythonAlgorithm):
 	  elif (bguess==0):
 		  for vi in range(0,len(voltlist)):
 	    # now the individual runs' spectra
-		    nfst0=runhandles[vi].getSampleDetails().getLogData("ICPEVENT").value
-		    gfi=nfst0.rfind("GF")
-		    gfie=nfst0.rfind("RF")
-		    nframes=int(nfst0[gfi+2:gfie])
+		    #nfst0=runhandles[vi].getSampleDetails().getLogData("ICPEVENT").value
+		    #gfi=nfst0.rfind("GF")
+		    #gfie=nfst0.rfind("RF")
+		    #nframes=int(nfst0[gfi+2:gfie])
+		    nframes=runhandles[vi].getSampleDetails().getLogData("goodfrm").value
 		    tres=runhandles[vi].readX(i)[2]-runhandles[vi].readX(i)[1]  # microseconds
-		    ff=mtd.createAlgorithm("Fit")
+		    #ff=mtd.createAlgorithm("Fit")
 		    nguess=runhandles[vi].dataY(i)[64]
-		    ff.setPropertyValue("Function","name=UserFunction, Formula=1/(1/(n*exp(-x/tau)+b)+d), n="+str(nguess)+", b=0.01, d=0.001, tau=2.19703")
-		    ff.setPropertyValue("InputWorkspace",runnames[vi])
-		    ff.setPropertyValue("WorkspaceIndex",str(i))
-		    ff.setPropertyValue("StartX",str(t1))
-		    ff.setPropertyValue("EndX","32")
-		    ff.setPropertyValue("Output","fitres")
+		    #ff.setPropertyValue("Function","name=UserFunction, Formula=1/(1/(n*exp(-x/tau)+b)+d), n="+str(nguess)+", b=0.01, d=0.001, tau=2.19703")
+		    #ff.setPropertyValue("InputWorkspace",runnames[vi])
+		    #ff.setPropertyValue("WorkspaceIndex",str(i))
+		    #ff.setPropertyValue("StartX",str(t1))
+		    #ff.setPropertyValue("EndX","32")
+		    #ff.setPropertyValue("Output","fitres")
 		    if(fml):
 			if(fbz):
-				ff.setPropertyValue("Ties","tau=2.19703,b=0.0")
+				#ff.setPropertyValue("Ties","tau=2.19703,b=0.0")
+				ties="tau=2.19703,b=0.0"
 			else:
-				ff.setPropertyValue("Ties","tau=2.19703")
+				#ff.setPropertyValue("Ties","tau=2.19703")
+				ties="tau=2.19703"
 		    else:
 			if(fbz):
-				ff.setPropertyValue("Ties","b=0.0")
-		    ff.execute()
-		    fr=mtd.getTableWorkspace("fitres_Parameters")
-		    Nfitted=fr.getDouble("Value",0)
-		    NfitE=fr.getDouble("Error",0)
-		    Bfitted=fr.getDouble("Value",2)
-		    BfitE=fr.getDouble("Error",2)
-		    Dfitted=fr.getDouble("Value",3)
-		    DfitE=fr.getDouble("Error",3)
+				#ff.setPropertyValue("Ties","b=0.0")
+				ties="b=0.0"
+			else:
+				ties=""
+		    #ff.execute()
+		    ff=Fit(Function="name=UserFunction, Formula=1/(1/(n*exp(-x/tau)+b)+d), n="+str(nguess)+", b=0.01, d=0.001, tau=2.19703",InputWorkspace=runnames[vi],WorkspaceIndex=str(i),StartX=str(t1),EndX=32,Output="fitres",Ties=ties)
+		    fr=mtd["fitres_Parameters"]
+		    Nfitted=fr.cell("Value",0)
+		    NfitE=fr.cell("Error",0)
+		    Bfitted=fr.cell("Value",2)
+		    BfitE=fr.cell("Error",2)
+		    Dfitted=fr.cell("Value",3)
+		    DfitE=fr.cell("Error",3)
 	    #print "fitted individual run ",vi," to give asym=",Afitted," backgd=",Bfitted," and deadtime=",Dfitted
 		    proc.dataX(i)[vi]=voltlist[vi]
 		    proc.dataY(i)[vi]=Nfitted/nframes/tres # counts/us/frame
@@ -247,10 +266,11 @@ class AnalyseThresholdScans(PythonAlgorithm):
 	  else:
 		  # integral counts only
 		  for vi in range(0,len(voltlist)):
-		    nfst0=runhandles[vi].getSampleDetails().getLogData("ICPEVENT").value
-		    gfi=nfst0.rfind("GF")
-		    gfie=nfst0.rfind("RF")
-		    nframes=int(nfst0[gfi+2:gfie])
+		    #nfst0=runhandles[vi].getSampleDetails().getLogData("ICPEVENT").value
+		    #gfi=nfst0.rfind("GF")
+		    #gfie=nfst0.rfind("RF")
+		    #nframes=int(nfst0[gfi+2:gfie])
+		    nframes=runhandles[vi].getSampleDetails().getLogData("goodfrm").value
 		    tres=runhandles[vi].readX(i)[2]-runhandles[vi].readX(i)[1]  # microseconds. Not needed for integral?
 		    sum=0
 		    hx=runhandles[vi].readX(i)
@@ -287,26 +307,27 @@ class AnalyseThresholdScans(PythonAlgorithm):
 	CropWorkspace(InputWorkspace="DTs",OutputWorkspace="DTs",XMin=0.0,XMax=998.0)
 # workspace "results" has 3 "time" bins, rate, mean pulse height and width
 	CloneWorkspace(InputWorkspace="rates",OutputWorkspace="results")
-	resw=mtd.getMatrixWorkspace("results")
+	resw=mtd["results"]
 # fit N versus volts to erf() or something similar
 # omit <25mV point2 from fit as it may have noise as well as positrons
 	for i in range(0,nhtp):
 	  #self.progress(0.95+0.05*i/proc.getNumberHistograms())
-	  ff=mtd.createAlgorithm("Fit")
-	  ff.setPropertyValue("Function","name=UserFunction, Formula=rate/(1+exp(x/lam - 2.75)), rate=5.0, lam=20.0")
-	  ff.setPropertyValue("InputWorkspace","rates")
-	  ff.setPropertyValue("StartX","24.0")
-	  ff.setPropertyValue("EndX","999.0")
-	  ff.setPropertyValue("Output","fitres")
+	  #ff=mtd.createAlgorithm("Fit")
+	  #ff.setPropertyValue("Function","name=UserFunction, Formula=rate/(1+exp(x/lam - 2.75)), rate=5.0, lam=20.0")
+	  #ff.setPropertyValue("InputWorkspace","rates")
+	  #ff.setPropertyValue("StartX","24.0")
+	  #ff.setPropertyValue("EndX","999.0")
+	  #ff.setPropertyValue("Output","fitres")
   #ff.setPropertyValue("Ties","PeakCentre=0.0")
-	  ff.setPropertyValue("WorkspaceIndex",str(i))
-	  ff.execute()
-	  fr=mtd.getTableWorkspace("fitres_Parameters")
+	  #ff.setPropertyValue("WorkspaceIndex",str(i))
+	  #ff.execute()
+	  ff=Fit(Function="name=UserFunction, Formula=rate/(1+exp(x/lam - 2.75)), rate=5.0, lam=20.0",InputWorkspace="rates",StartX="24.0",EndX="999.0",Output="fitres",WorkspaceIndex=str(i))
+	  fr=mtd["fitres_Parameters"]
 	  for j in range(0,3):
 	    resw.dataX(i)[j]=j*1.0
-	    resw.dataY(i)[j]=fr.getDouble("Value",j)
+	    resw.dataY(i)[j]=fr.cell("Value",j)
 	    try:
-	      resw.dataE(i)[j]=fr.getDouble("Error",j)
+	      resw.dataE(i)[j]=fr.cell("Error",j)
 	    except:
 	      print "missing Error column from fit"
 	      resw.dataE(i)[j]=0.0
@@ -320,4 +341,4 @@ class AnalyseThresholdScans(PythonAlgorithm):
 	#print "Finished!"
 # open Instrument View?
 
-mantid.registerPyAlgorithm(AnalyseThresholdScans())
+AlgorithmFactory.subscribe(AnalyseThresholdScans)
