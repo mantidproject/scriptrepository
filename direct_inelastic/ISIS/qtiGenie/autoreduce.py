d4258955feb60f3fba10cd8ca060b6b17e9a171e
Alex Buts <Alex.Buts@stfc.ac.uk>
2014-03-18 19:33:34 +0000

    made autoreduce generic and moved it into qtiGenie
    it may go back to insrument as currently this autoreduce is rather LET specific

--- direct_inelastic/ISIS/qtiGenie/autoreduce.py
+++ direct_inelastic/ISIS/qtiGenie/autoreduce.py
@@ -17,23 +17,24 @@ dataFileName = os.path.split(dataFile)[-1]
 dataFilePath = dataFile.replace(dataFileName, '')
 dataFileNameMinuxExt = dataFileName.split('.')[0]
 runNumber = int(re.findall('\d+', dataFileNameMinuxExt)[0])
-instrument = dataFilePath.lower().split('/ndx')[1].split('/')[0]
+InstrName = dataFilePath.lower().split('/ndx')[1].split('/')[0]
+#InstrName ='LET'
 #############################################
 
 print 'data file path: ' + dataFilePath
 sys.path.append(dataFilePath)
 
 from qtiGenie import *
-iliad_setup(instrument)
+iliad_setup(InstrName)
 
 # program to crunch down event mode from LET and produce output SPE files. Program can automatically find all incident energies in rep rate mode and write out spe files in the # form of LET'run no: +ei'.spe
 
 #############################################
 # this is the user input section
-wb=14392   # enter whitebeam run number here (cycle 2013/3)
-#ei = [10.2,4.0,2.13,1.31]  #ei=[5.8,15]           # incident energies you want analysed, or leave as ei=[]  if you want all incident energies analysed
-ei = [4.0]  #ei=[5.8,15]           # incident energies you want analysed, or leave as ei=[]  if you want all incident energies analysed
-#ei = [30.6,10.2,5.01,2.97]  #ei=[5.8,15]           # incident energies you want analysed, or leave as ei=[]  if you want all incident energies analysed
+wb=11869 #14392   # enter whitebeam run number here (cycle 2013/3)
+#ei = [10.2,4.0,2.13,1.31]  #ei=[5.8,15]           # incident energies you want analyzed, or leave as ei=[]  if you want all incident energies analyzed
+ei = [3.4]  #ei=[5.8,15]           # incident energies you want analyzed, or leave as ei=[]  if you want all incident energies analyzed
+#ei = [30.6,10.2,5.01,2.97]  #ei=[5.8,15]           # incident energies you want analyzed, or leave as ei=[]  if you want all incident energies analyzed
 ebin=[-0.8,0.002,0.9]    #binning of the energy for the spe file. The numbers are as a fraction of ei [from ,step, to ]
 mapping='LET_rings_133'  # rings mapping file for powders
 file = 'hard_133.msk'    # standard hard mask file  for LET
@@ -43,12 +44,14 @@ file = 'hard_133.msk'    # standard hard mask file  for LET
 #file = '/home/let/Desktop/LET_maps/hard_14Tmagnet.msk'  #14T mask
 #file = '/home/let/Desktop/LET_maps/magnet7T_hard.msk'    #7T mask
 ############################################
-# currently done here on-
+
+
+# currently done here on -- will go to iliad later
 remove_background = False  #if true then will subtract a flat background in time from the time range given below otherwise put False
 bg_range=[92000,98000] # range of times to take background in
 
 # Absolute units reduction MonoVanRun=None disables it. Sample and vanadium mass parameters have to be right otherwise
-# Vanadium labelled Dec 2011 - flat plate of dimensions: 40.5x41x2.0# volume = 3404.025 mm**3 mass= 20.79
+# Vanadium labeled Dec 2011 - flat plate of dimensions: 40.5x41x2.0# volume = 3404.025 mm**3 mass= 20.79
 sampleMass=20.79 # 17.25  # mass of your sample (PrAl3)
 sampleRMM= 50.9415 # 221.854  # molecular weight of your sample
 MonoVanRun=None # vanadium run in the same configuration as your sample
@@ -58,12 +61,17 @@ monovan_mapfile='rings_103.map'  #
 
 wbFile= dataFilePath + 'LET000'+str(wb)+'.raw'
 # White beam
-if 'wb_wksp' in mtd and wb.getRunNumber()==wb:
+loadFreshWB=True;
+if 'wb_wksp' in mtd:
     wb_wksp=mtd['wb_wksp']
-else:  #only load whitebeam if not already there
+    if wb_wksp.getRunNumber()==wb:
+        loadFreshWB = False;
+#only load whitebeam if not already there
+if loadFreshWB:  
     wb_wksp=LoadRaw(Filename=wbFile,OutputWorkspace="wb_wksp",LoadMonitors='Exclude')
 
 ######################################################################
+
 
 print ' processing file ', dataFile
 #w1 = dgreduce.getReducer().load_data(run,'w1')
@@ -71,9 +79,9 @@ Load(Filename=dataFile,OutputWorkspace='w1',LoadMonitors='1');
 
     
 if remove_background:
-    bg_ws_name=find_background('w1',bg_range,1.6);
+    bg_ws_name=find_background('w1',bg_range);
 
-	
+    
 ##################################300
 # this section finds all the transmitted incident energies
 if len(ei) == 0:
@@ -93,6 +101,10 @@ argi['norm_method']='current'
 argi['det_cal_file']='det_LET_cycle133.dat'
 argi['detector_van_range']=[0.5,200]
 argi['hardmaskOnly']=file
+argi['bkgd_range']=[bg_range[0],bg_range[1]]
+# background removal range  -- used for standard background removal
+#argi['bkgd_range']=[int(t_elastic),int(tmax)]
+  
 # abs units
 argi['sample_mass']=sampleMass;
 argi['sample_rmm'] =sampleRMM;
@@ -101,17 +113,12 @@ argi['monovan_mapfile']=monovan_mapfile;
 
 for ind,energy in enumerate(ei):
     print "Reducing around energy: {0}".format(float(energy))
-    (energybin,tbin,t_elastic) = find_binning_range(energy,ebin,25.,23.5,4.1,1.6);
+    # Instrument Specific parameters used
+    (energybin,tbin,t_elastic) = find_binning_range(energy,ebin);
                        
-    energybin=[ebin[0]*energy,ebin[1]*energy,ebin[2]*energy]
-    energybin = [ '%.4f' % elem for elem in energybin ] 
-    ebinstring=str(energybin[0])+','+str(energybin[1])+','+str(energybin[2])
-    print ebinstring
 
-    # background removal range  -- used for standard background removal
-    argi['bkgd_range']=[int(t_elastic),int(tmax)]
     # abs units
-    argi['monovan_integr_range']=[round(ebin[0]*energy,4),round(ebin[2]*energy,4)]; # integration range of the vanadium 
+    argi['monovan_integr_range']=[energybin[0],energybin[2]]; # integration range of the vanadium 
 
     # if we calculate more then one energy, initial workspace will be used more then once. Is this enough for that?    
     #Rebin(InputWorkspace='w1',OutputWorkspace='w1reb',Params=tbin,PreserveEvents='1')                
@@ -124,11 +131,11 @@ for ind,energy in enumerate(ei):
         RenameWorkspace(InputWorkspace = 'w1_mon_storage',OutputWorkspace='w1_monitors');
     
     if remove_background:
+        w1=Rebin(InputWorkspace='w1',OutputWorkspace='w1',Params=tbin,PreserveEvents=False)
         Minus(LHSWorkspace='w1',RHSWorkspace=bg_ws_name,OutputWorkspace='w1')
         
-		
     # absolute unit reduction -- if you provided MonoVan run or relative units if monoVan is not present
-    out=iliad("wb_wksp","w1reb",energy,energybin,mapping,MonoVanRun,**argi);
-    file_name = 'LET{0}_reducedEi{1:4.2f}_mev.nxspe'.format(runNumber,energy);
+    out=iliad(wb_wksp,"w1",energy,energybin,mapping,MonoVanRun,**argi);
+    file_name = '{0}{1}_reducedEi{2:5.2f}_mev.nxspe'.format(InstrName,runNumber,energy);
     SaveNXSPE(InputWorkspace=out,Filename=file_name);
 
