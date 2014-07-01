4b41ab46480c677478fc0030bd289593d5d6ed69
Robert Dalgliesh <robert.dalgliesh@stfc.ac.uk>
2014-07-01 11:44:43 +0100

    modifications for debugging

--- sans/Larmor/Larmor.py
+++ sans/Larmor/Larmor.py
@@ -1,5 +1,6 @@
 from mantid.simpleapi import *
 from shutil import copyfile
+from math import *
 import nxs as nxs
 import os
 from ISISCommandInterface import *
@@ -63,7 +64,7 @@ def parseRunList(istring):
 SANS add runs from RKH
 '''
 
-def add_runs(runlist,pathout,instrument='LARMOR'):
+def add_runs(runlist,pathout,instrument='LARMOR',keepwksp=0,savewksp=1):
 	if(os.path.isdir(pathout)=='False'):
 		return
 	pfix=instrument
@@ -79,14 +80,19 @@ def add_runs(runlist,pathout,instrument='LARMOR'):
 			added=added+wtemp
 		DeleteWorkspace("wtemp")
 	
-	nzeros=8-len(runlist[0])
-	fpad=""
-	for ii in range(nzeros):
-		fpad+="0"
-	print "writing file:   "+pathout+pfix+fpad+runlist[0]+"-add.nxs"
-	SaveNexusProcessed("added",pathout+pfix+fpad+runlist[0]+"-add.nxs")  
-	print "    total uampHr = ", added.getRun().getProtonCharge()
-	DeleteWorkspace("added")
+	if savewksp==1:
+		nzeros=8-len(runlist[0])
+		fpad=""
+		for ii in range(nzeros):
+			fpad+="0"
+		print "writing file:   "+pathout+pfix+fpad+runlist[0]+"-add.nxs"
+		SaveNexusProcessed("added",pathout+pfix+fpad+runlist[0]+"-add.nxs")  
+		print "    total uampHr = ", added.getRun().getProtonCharge()
+	if keepwksp==0:
+		DeleteWorkspace("added")
+	else:
+		RenameWorkspace("added",OutputWorkspace=runlist[0]+'-add')
+	return mtd[runlist[0]+'-add']
 
 def larmor1D(rsample,rcan,tsample,tdb,tcan,maskfile,wkspname,lmin=0.9,lmax=12.5):
 	LARMOR()
@@ -109,12 +115,13 @@ def calibrateTubes(wkspName,calibrationfile):
    #
    # pixel by pixel efficiency correction for the linear detector
    #
-   flood_wksp = "Vanadium_tube_calib_1to1_25062014"
+   flood_wksp = "Vanadium_tube_calib_1to1_30062014"
    if  flood_wksp not in mtd:
 		#Load("Vanadium_tube_calib_1to1_May14.nxs",OutputWorkspace="Vanadium_tube_calib_1to1_May14")
-		Load("Vanadium_tube_calib_1to1_25062014.nxs",OutputWorkspace="Vanadium_tube_calib_1to1_25062014")
+		#Load("Vanadium_tube_calib_1to1_25062014.nxs",OutputWorkspace="Vanadium_tube_calib_1to1_25062014")
+		Load("Vanadium_tube_calib_1to1_30062014.nxs",OutputWorkspace="Vanadium_tube_calib_1to1_30062014")
 
-   CopyInstrumentParameters("Vanadium_tube_calib_1to1_25062014",wkspName)
+   CopyInstrumentParameters("Vanadium_tube_calib_1to1_30062014",wkspName)
 
 def virtualslitxml(ypos):
 	# Active detector length is ~640mm
@@ -133,7 +140,7 @@ def createMapFile(rnum,fname):
 	e.g. createMapFile(1203,'w:\Users\Masks\Integrate_tubes_map_file.xml')
 	'''
 	w1=Load(str(rnum))
-	lm.calibrateTubes(w1,"")
+	calibrateTubes(w1,"")
 	det=CropWorkspace(w1,StartWorkspaceIndex=10)
 	f=open(fname,'w')
 	s='<?xml version="1.0" encoding="UTF-8" ?>\n'
@@ -157,26 +164,28 @@ def createMapFile(rnum,fname):
 	f.write(s)
 	f.close()
 
-def create1DPSD(rnum,lmin=1.0,lmax=12.0,Mapfile='w:\Users\Masks\Integrate_tubes_map_file.xml'):
+def create1DPSD(rnum,lmin=0.8,lmax=13.0,Mapfile='w:\Users\Masks\Integrate_tubes_map_file.xml'):
 	'''
 	Sum the detector in horizontal stripes according to the setup within a mapfile
 	'''
 	w1=Load(str(rnum))
-	lm.calibrateTubes(w1,"")
+	calibrateTubes(w1,"")
 	w1=ConvertUnits(w1,'Wavelength',AlignBins=1)
-	w2=Integration(w1,RangeLower=lmin,RangeUpper=lmax)
-	w3=CropWorkspace(w2,StartWorkspaceIndex=0,EndWorkspaceIndex=0)
-	detgrp=GroupDetectors(w2,MapFile=Mapfile)
+	w1=CropWorkspace(w1,lmin,lmax)
+	w2=CropWorkspace(w1,StartWorkspaceIndex=0,EndWorkspaceIndex=0)
+	detgrp=GroupDetectors(w1,MapFile=Mapfile)
 	detgrp=CropWorkspace(detgrp,StartWorkspaceIndex=10)
-	w5=detgrp/w3
+	w3=detgrp/w2
+	w4=Integration(w3,RangeLower=lmin,RangeUpper=lmax)
 	x=[]
 	y=[]
 	e=[]
 	pixelsize=0.64/500.0
 	for i in range(500):
 		x.append(-0.32-0.5*pixelsize+i*pixelsize)
-		y.append(w5.readY(i)[0])
-		e.append(w5.readE(i)[0])
+		y.append(w4.readY(i)[0])
+		e.append(w4.readE(i)[0])
 	x.append(-0.32-0.5*pixelsize+500*pixelsize)
 	CreateWorkspace(x,y,e,1,OutputWorkspace=str(rnum)+'_d_h')
+	RenameWorkspace(w3,OutputWorkspace=str(rnum)+'_1DPSD')
 	return [x,y,e]
