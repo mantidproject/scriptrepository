791191c7a2b0e541c53319ed9dcb6cf7566e8080
Arthur Schultz <ajschultz2@gmail.com>
2015-02-09 18:26:23 +0000

    MantidPlot script to find, index and integrate peaks.

---
+++ SingleCrystal/MantidPlot_scripts/AnalyzeOneRun.py
@@ -0,0 +1,30 @@
+# Initial analysis to find peaks and to obtain lattice constants and UB matrix.
+LoadEventNexusDialog(OutputWorkspace = 'Event_ws', FilterByTofMin=500, FilterByTofMax=16500, Precount=False, 
+    Enable = 'OutputWorkspace, FilterByTofMin, FilterByTofMax')
+ConvertToDiffractionMDWorkspaceDialog(InputWorkspace='event_ws', OutputWorkspace='MD_ws', 
+    OneEventPerBin=False, LorentzCorrection=True, SplitThreshold=50, MaxRecursionDepth=11, 
+    Enable ='InputWorkspace, OutputWorkspace, LorentzCorrection')
+FindPeaksMDDialog(InputWorkspace='MD_ws', OutputWorkspace='Peaks', MaxPeaks = 500, 
+    Enable = 'InputWorkspace, OutputWorkspace, MaxPeaks')
+FindUBUsingFFTDialog(PeaksWorkspace='Peaks', MinD=5, MaxD=13, Tolerance=0.12, Enable = 'PeaksWorkspace, MinD, MaxD, Tolerance')
+IndexPeaksDialog(PeaksWorkspace='Peaks', Tolerance = 0.12, RoundHKL=True, Enable = 'PeaksWorkspace, Tolerance, RoundHKL')
+SaveIsawUBDialog(InputWorkspace = 'Peaks', Enable = 'InputWorkspace')
+SaveIsawPeaksDialog(InputWorkspace = 'Peaks', Enable = 'InputWorkspace')
+
+# Create MD workspace with Lorentz correction turned off for integration.
+ConvertToDiffractionMDWorkspaceDialog(InputWorkspace='event_ws', OutputWorkspace='MD_ws', 
+    OneEventPerBin=False, LorentzCorrection=False, SplitThreshold=50, MaxRecursionDepth=11, 
+    Enable ='InputWorkspace, OutputWorkspace, LorentzCorrection')
+    
+IntegratePeaksMDDialog(InputWorkspace = 'MD_ws', PeaksWorkspace = 'Peaks',  OutputWorkspace = 'IntegratedPeaks_MD', PeakRadius = 0.10, 
+    BackgroundInnerRadius = 0.10, BackgroundOuterRadius = 0.12, Cylinder = False)
+SaveIsawPeaksDialog(InputWorkspace = 'IntegratedPeaks_MD', Enable = 'InputWorkspace')
+    
+IntegratePeaksHybridDialog(InputWorkspace = 'MD_ws', PeaksWorkspace = 'Peaks', OutputWorkspace = 'IntegratedPeaks_Hybrid',
+    BackgroundOuterRadius = 0.15, OutputWorkspaces = 'MDHisto')
+SaveIsawPeaksDialog(InputWorkspace = 'IntegratedPeaks_Hybrid', Enable = 'InputWorkspace')
+
+# BinMDDialog(InputWorkspace = 'MD_ws', OutputWorkspace = 'BinMD_ws')
+# IntegratePeaksUsingClustersDialog(InputWorkspace = 'BinMD_ws', PeaksWorkspace = 'Peaks', Threshold = 10, 
+    # OutputWorkspace = 'IntegratedPeaks_Cluster', OutputWorkspaceMD = 'MD_out')
+
