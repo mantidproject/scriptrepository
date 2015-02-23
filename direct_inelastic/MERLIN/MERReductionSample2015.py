0340f430d8f18826fb6412b1d43570a1a7e3c72f
Alex Buts <Alex.Buts@stfc.ac.uk>
2015-02-23 13:15:19 +0000

    Sample reduction script for MERLIN working from PC and web services alike

---
+++ direct_inelastic/MERLIN/MERReductionSample2015.py
@@ -0,0 +1,197 @@
+""" Sample MER reduction script """ 
+# Two rows necessary to run script outside of the mantid. You need also set up 
+# appropriate python path-es
+import os
+#os.environ["PATH"] = r"c:/Mantid/Code/builds/br_master/bin/Release;"+os.environ["PATH"]
+ 
+
+from Direct.ReductionWrapper import *
+# necessary for web services to work.  Will go with factory implemented
+try:
+    import reduce_vars as web_var
+except:
+    web_var = None
+
+class ReduceMER_MultiRep2015(ReductionWrapper):
+   @MainProperties
+   def def_main_properties(self):
+       """ Define main properties used in reduction. These are the property 
+           a user usually wants to change
+       """ 
+       prop = {}
+
+       ei=[81,30,15] # multiple energies provided in the data file
+       ebin=[-0.1,0.005,0.95]    #binning of the energy for the spe file. The numbers are as a fraction of ei [from ,step, to ]
+       # the range of files to reduce. This range ignored when deployed from autoreduction, unless you going to sum these files
+       # used when you run reduction from PC
+       prop['sample_run'] = range(22413,22932)
+       prop['wb_run'] = 23012
+       prop['incident_energy'] = ei
+       prop['energy_bins'] = ebin
+       prop['sum_runs'] = False # set to true to sum everything provided to sample_run
+                                # list      
+      # Absolute units reduction properties.
+       prop['monovan_run'] = 22932 # vanadium run in the same configuration as your sample
+       prop['sample_mass'] = 9 # mass of your sample 
+       prop['sample_rmm'] = 496.4 # molecular weight of scatterers in your sample
+       prop['sum_runs']=False
+       return prop
+#------------------------------------------------------------------------------------#
+   @AdvancedProperties
+   def def_advanced_properties(self):
+      """  Set up advanced properties, describing reduction.
+           These are the properties, usually provided by an instrument 
+           scientist
+            
+           separation between simple and advanced properties depends
+           on scientist, experiment and user.   All are necessary for reduction 
+           to work properly
+      """
+      prop = {}
+      prop['map_file'] = 'one2one_125.map'
+      prop['det_cal_file'] = 'det_corrected7.nxs'
+      prop['bleed'] = False
+      prop['norm_method']='current'
+      prop['detector_van_range']=[40,55]
+      prop['background_range'] = [12000,19000] # TOF range for the calculating flat background
+      prop['hardmaskOnly']='Bjorn_mask.msk' # diag does not work well on MER. At present only use a hard mask RIB has created
+
+      prop['check_background']=False
+
+      prop['save_format'] = '' #nxs,nxspe'
+       # if two input files with the same name and  different extension found, what to prefer. 
+      prop['data_file_ext']='.nxs' # for MER it may be choice between event and histo mode if 
+      # raw file is written in histo, and nxs -- in event mode
+                                    
+      prop['monovan_mapfile'] = 'rings_125.map'
+      prop['vanadium-mass']=7.85 # check this
+      return prop
+      #
+#------------------------------------------------------------------------------------#
+   @iliad
+   def reduce(self,input_file=None,output_directory=None):
+      """ Method executes reduction over single file
+
+          Overload only if custom reduction is needed or 
+          special features are requested
+      """
+      ws = ReductionWrapper.reduce(self,input_file,output_directory)
+      #SaveNexus(ws,Filename = 'MARNewReduction.nxs')
+      return ws
+   #
+   def validate_result(self,build_validation=False,Error=1.e-3,ToleranceRelErr=True):
+      """ Change this method to verify different results     """
+      # here we have: 
+      #  14305                    run number with known reduction result
+      # sample_map21384_ei450.nxs workspace for run above reduced earlier and we now 
+      # validate against
+      # build_validation -- if true, build and save new workspace rather
+      #then validating the old one
+      run_dir = os.path.dirname(os.path.realpath(__file__))
+      # this row defines location of the validation file in this script folder
+      validation_file = os.path.join(run_dir,"MER22413Ei81meV_Abs.nxs")
+      rez,message = ReductionWrapper.build_or_validate_result(self,22413,
+                                     validation_file,build_validation,
+                                     Error,ToleranceRelErr)
+      return rez,message
+   #
+   def set_custom_output_filename(self):
+      """ define custom name of output files if standard one is not satisfactory 
+          In addition to that, example of accessing reduction properties 
+          Changing them if necessary
+      """ 
+      def custom_name(prop_man):
+          """ sample function which builds filename from 
+              incident energy and run number and adds some auxiliary information 
+              to it.
+          """ 
+          # Note -- properties have the same names  as the list of advanced and 
+          # main properties
+          ei = prop_man.incident_energy
+          # sample run is more then just list of runs, so we use 
+          # the formalization below to access its methods
+          run_num = PropertyManager.sample_run.run_number()
+          name = "RUN{0}atEi{1:<4.1f}meV_One2One".format(run_num ,ei)
+          return name
+       
+      # Uncomment this to use custom filename function
+      # Note: the properties are stored in prop_man class accessed as
+      # below. 
+      #return custom_name(self.reducer.prop_man)
+      # use this method to use standard file name generating function
+      return None    
+
+#------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------#
+def main(input_file=None,output_directory=None):
+    """ This method is used to run code from web service
+        and should not be touched unless you change the name of the
+        particular ReductionWrapper class (e.g. ReduceMAPS here)
+
+        exception to change the output folder to save data to
+    """
+    # note web variables initialization
+    rd = ReduceMER_MultiRep2015(web_var)
+    rd.reduce(input_file,output_directory)
+    
+    # Define folder for web service to copy results to
+    output_folder = ''
+    return output_folder
+
+if __name__ == "__main__":
+#------------------------------------------------------------------------------------#
+# SECTION USED TO RUN REDUCTION FROM MANTID SCRIPT WINDOW #
+#------------------------------------------------------------------------------------#
+##### Here one sets up folders where to find input data and where to save results ####
+    # It can be done here or from Mantid GUI:
+    #      File->Manage user directory ->Browse to directory
+    # Folder where map and mask files are located:
+    map_mask_dir = 'c:/Users/wkc26243/Documents/work/Libisis/InstrumentFiles/merlin'
+    # folder where input data can be found
+    data_dir = 'd:/Data/Mantid_Testing/15_01_27/merlin'
+    # auxiliary folder with results
+    ref_data_dir = 'd:/Data/MantidSystemTests/Data' 
+    # Set input search path to values, specified above
+    config.setDataSearchDirs('{0};{1};{2}'.format(data_dir,map_mask_dir,ref_data_dir))
+    # use appendDataSearch directory to add more locations to existing Mantid 
+    # data search path
+    #config.appendDataSearchDir('d:/Data/Mantid_GIT/Test/AutoTestData')
+    # folder to save resulting spe/nxspe files.
+    config['defaultsave.directory'] = data_dir 
+
+###### Initialize reduction class above and set up reduction properties.        ######
+######  Note no web_var in constructor.(will be irrelevant if factory is implemented)
+    rd = ReduceMER_MultiRep2015()
+    # set up advanced and main properties
+    rd.def_advanced_properties()
+    rd.def_main_properties()
+
+#### uncomment rows below to generate web variables and save then to transfer to   ###
+    ## web services.
+    run_dir = os.path.dirname(os.path.realpath(__file__))
+    file = os.path.join(run_dir,'reduce_vars.py')
+    rd.save_web_variables(file)
+
+#### Set up time interval (sec) for reducer to check for input data file.         ####
+    #  If this file is not present and this value is 0,reduction fails 
+    #  if this value >0 the reduction wait until file appears on the data 
+    #  search path checking after time specified below.
+    rd.wait_for_file = 0  # waiting time interval
+
+####get reduction parameters from properties above, override what you want locally ###
+   # and run reduction. Overriding would have form:
+   # rd.reducer.prop_man.property_name (from the dictionary above) = new value e.g. 
+   # rd.reducer.prop_man.energy_bins = [-40,2,40]
+   # or 
+   ## rd.reducer.prop_man.sum_runs = False
+   # 
+    rd.run_reduction()
+
+#### Validate results against known result, obtained earlier -- filename defined by ##
+    # get_validation_file_name method
+#    rez,mess=rd.validate_result()
+#    if not rez:
+#      raise RuntimeError("validation failed with error: {0}".format(mess))
+#   else:
+#     print "ALL Fine" 
