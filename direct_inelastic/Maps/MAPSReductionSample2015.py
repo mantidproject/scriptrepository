9b9825ef022ad28e063d06d89b886238faa4c32e
Alex Buts <Alex.Buts@stfc.ac.uk>
2015-02-24 13:31:13 +0000

    Sample MAPS reduction script  for 2015 runs

--- direct_inelastic/Maps/MAPSReductionSample2015.py
+++ direct_inelastic/Maps/MAPSReductionSample2015.py
@@ -2,8 +2,8 @@
 # Two rows necessary to run script outside of the mantid. You need also set up 
 # appropriate python path-es
 import os
-os.environ["PATH"] = r"c:/Mantid/Code/builds/br_master/bin/Release;"+\
-                     os.environ["PATH"]
+#os.environ["PATH"] = r"c:/Mantid/Code/builds/br_master/bin/Release;"+\
+#                     os.environ["PATH"]
 #
 from Direct.ReductionWrapper import *
 # necessary for web services to work.  Will go with factory implemented
@@ -14,24 +14,31 @@ except:
 
 class ReduceMAPS(ReductionWrapper):
 #------------------------------------------------------------------------------------#
-   @MainProperties
+   @MainProperties	
    def def_main_properties(self):
        """ Define main properties used in reduction. These are the property 
            a user usually wants to change
        """ 
        prop = {}
-       # the range of files to reduce. This range ignored when deployed from autoreduction, unless you going to sum these files
-       # used when you run reduction from PC      
-       prop['sample_run'] = [21384,21385]
-       prop['wb_run'] = 21376
+       # if energy is specified as a list (even with single value e.g. ei=[81])
+       # The numbers are treated as a fraction of ei [from ,step, to ]. If energy is 
+	   # a number, energy binning assumed to be absolute (e_min, e_step,e_max)
+       #
        prop['incident_energy'] = 450
        prop['energy_bins'] = [-50,2.5,425]
+	   #
+       # the range of files to reduce. This range ignored when deployed from autoreduction,
+       # unless you going to sum these files. 
+       # The range of numbers or run number is used when you run reduction from PC.
+       prop['sample_run'] = [21384,21385]
+       prop['wb_run'] = 21376
+       #
        prop['sum_runs'] = False # set to true to sum everything provided to sample_run
-                                # list
-      # Absolute units reduction properties.
-       prop['monovan_run'] = 21803  # set None to do arbitrary units
-       prop['sample_mass'] = 41.104
-       prop['sample_rmm'] = 398.9439
+       #                        # list
+       # Absolute units reduction properties. Set prop['monovan_run']=None to do relative units
+       prop['monovan_run'] = 21803  #  vanadium run in the same configuration as your sample 
+       #prop['sample_mass'] = 41.104
+       #prop['sample_rmm'] = 398.9439
        return prop
 #------------------------------------------------------------------------------------#
    @AdvancedProperties
@@ -96,10 +103,31 @@ class ReduceMAPS(ReductionWrapper):
       return rez,message
    #
    def set_custom_output_filename(self):
-      """ define custom name of output files if standard one is not satisfactory """ 
+      """ define custom name of output files if standard one is not satisfactory 
+          In addition to that, example of accessing reduction properties 
+          Changing them if necessary
+      """ 
+      def custom_name(prop_man):
+          """ sample function which builds filename from 
+              incident energy and run number and adds some auxiliary information 
+              to it.
+          """ 
+          # Note -- properties have the same names  as the list of advanced and 
+          # main properties
+          ei = prop_man.incident_energy
+          # sample run is more then just list of runs, so we use 
+          # the formalization below to access its methods
+          run_num = PropertyManager.sample_run.run_number()
+          name = "RUN{0}atEi{1:<4.1f}meV_One2One".format(run_num ,ei)
+          return name
+       
+      # Uncomment this to use custom filename function
+      # Note: the properties are stored in prop_man class accessed as
+      # below. 
+      #return custom_name(self.reducer.prop_man)
+      # use this method to use standard file name generating function
       return None
-    
-
+   #
    def __init__(self,web_var=None):
        """ sets properties defaults for the instrument with Name"""
        ReductionWrapper.__init__(self,'MAP',web_var)
