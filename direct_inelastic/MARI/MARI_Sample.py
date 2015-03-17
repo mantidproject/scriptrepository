95260c6cde457d1165d6a1c11c488cb0ca892f48
Alex Buts <Alex.Buts@stfc.ac.uk>
2015-03-17 11:03:49 +0000

    Sample reduction script for MARI, supposed to be used on servers from 2015 onwards.

--- direct_inelastic/MARI/MARI_Sample.py
+++ direct_inelastic/MARI/MARI_Sample.py
@@ -80,7 +80,6 @@ class ReduceMARIFromFile(ReductionWrapper):
 
         outWS = ReductionWrapper.reduce(self,input_file,output_directory)
         #SaveNexus(outWS,Filename = 'MARNewReduction.nxs')
-        # Does the possibility to return PATH where to copy from web-services exist?
         return outWS
 
     def validate_result(self,build_validation=False):
@@ -188,12 +187,12 @@ if __name__ == "__main__":
     # of workspace pointers.
     #red_ws = rd.run_reduction()
     # usual way to go is to reduce workspace and save it internally
-    #rd.run_reduction()
+    rd.run_reduction()
 
 
 #### Validate reduction result against known result, obtained earlier  ###
-    rez,mess=rd.validate_result()
-    if not rez:
-       raise RuntimeError("validation failed with error: {0}".format(mess))
-    else:
-       print "ALL Fine"
+#    rez,mess=rd.validate_result()
+#   if not rez:
+#       raise RuntimeError("validation failed with error: {0}".format(mess))
+#   else:
+#       print "ALL Fine"
