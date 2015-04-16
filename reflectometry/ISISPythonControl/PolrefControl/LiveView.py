d3ca0333136290a3120ed5d7e6ce8aea7c281111
TRC <timothy.charlton@stfc.ac.uk>
2015-04-16 09:57:10 +0100

    .

---
+++ reflectometry/ISISPythonControl/PolrefControl/LiveView.py
@@ -0,0 +1,13 @@
+import os, sys
+sys.path.append(r'C:\MantidInstall')
+sys.path.append(r'C:\MantidInstall\bin')
+sys.path.append(r'u:\mantid\TestingPointDetectorReduction')
+sys.path.append(r'u:\mantid')
+from PlotScan import PlotScan, ChopperChecks
+from mantid.simpleapi import *
+from WrappedReduction import *
+
+r=Runs()
+
+r2l=refl(r[0],twotheta=4.0);
+r2l=Rebin(r2l, '2.5,0.05,11')
