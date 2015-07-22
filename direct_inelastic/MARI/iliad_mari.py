5f8419ed1c8cb2cd6671958f63a20c59192ee83e
Alex Buts <Alex.Buts@stfc.ac.uk>
2015-07-22 16:30:31 +0100

    changes from MARI (went long way)

--- direct_inelastic/MARI/iliad_mari.py
+++ direct_inelastic/MARI/iliad_mari.py
@@ -5,6 +5,7 @@ from mantid import config
 reload(mpr)
 rd = mpr.MARIReduction()
 
+
 # set up advanced and main properties
 rd.def_advanced_properties()
 rd.def_main_properties()
@@ -12,16 +13,16 @@ rd.def_main_properties()
 #Filename?
 rd.set_custom_output_filename()
 
-def iliad_mari(runno,ei,wbvan,monovan,sam_mass,sam_rmm):
+def iliad_mari(runno,ei,wbvan,monovan,sam_mass,sam_rmm,sum_runs=False):
 
     rd.reducer.prop_man.map_file="mari_res2013.map"
     rd.reducer.prop_man.hard_mask_file = "mari_mask2015.msk"
     
     rd.reducer.prop_man.incident_energy=ei
-    
+    rd.reducer.prop_man.sum_runs= sum_runs
     rd.reducer.prop_man.sample_run = runno
     rd.reducer.prop_man.wb_run=wbvan
-    rd.reducer.prop_man.energy_bins=[-ei,ei/100.,0.95*ei]
+    rd.reducer.prop_man.energy_bins=[-0.5*ei,ei/200.,0.97*ei]
     
     if ( sam_rmm!=0 and sam_mass!=0 ) :
         abs_units=1
