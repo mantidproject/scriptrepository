b4c843dc20e99785727a3a45b26ded614e67555e
Owen Arnold <owen.arnold@gmail.com>
2016-01-19 17:21:21 +0000

    Some other useful comments

--- user/OwenArnold/smooth_script.py
+++ user/OwenArnold/smooth_script.py
@@ -1,3 +1,4 @@
+# Some useful comments
 ws = CreateMDWorkspace(Dimensions=2, Extents=[-10,10,-10,10], Names='A,B', Units='U,U')
 FakeMDEventData(InputWorkspace=ws, PeakParams='100000,-5,0,1')
 FakeMDEventData(InputWorkspace=ws, PeakParams='100000,5,0,1')
