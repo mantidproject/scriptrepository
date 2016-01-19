f314f5d4ea619beff526ea03c057438ded18dcb4
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-01-19 17:19:36 +0000

    version 1

---
+++ user/OwenArnold/smooth_script.py
@@ -0,0 +1,7 @@
+ws = CreateMDWorkspace(Dimensions=2, Extents=[-10,10,-10,10], Names='A,B', Units='U,U')
+FakeMDEventData(InputWorkspace=ws, PeakParams='100000,-5,0,1')
+FakeMDEventData(InputWorkspace=ws, PeakParams='100000,5,0,1')
+histogram = BinMD(InputWorkspace='a', AlignedDim0='A,-10,10,100', AlignedDim1='B,-10,10,100', OutputExtents='-10,10,-10,10,-10,10', OutputBins='10,10,10')
+#plotSlice(histogram)
+smoothed = SmoothMD(InputWorkspace=histogram, WidthVector=5, Function='Hat')
+#plotSlice(smoothed)
