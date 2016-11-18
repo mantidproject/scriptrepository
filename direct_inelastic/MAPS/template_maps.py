11e420ec187156859be6ef1319658c9a9910ce6d
Alex Buts <Alex.Buts@stfc.ac.uk>
2016-11-18 10:42:37 +0000

    Changed template to insert RB folder on python search path first

--- direct_inelastic/MAPS/template_maps.py
+++ direct_inelastic/MAPS/template_maps.py
@@ -7,6 +7,7 @@ import sys
 try:
     #Note: due to the mantid-python implementation, one needs to run this 
     #script in Mantid  script window  TWICE!!!  to deploy  the the changes made to MAPSReduction_Sample.py file.
+    sys.path.insert(0,'/instrument/MARI/RBNumber/USER_RB_FOLDER');    
     reload(sys.modules['MAPSReduction_Sample'])
 except:
     print "*** WARNING can not reload MAPSReduction_Sample file"
