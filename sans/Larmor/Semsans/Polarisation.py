5f46257e3b4467064edf22f786f4a074afce8e15
Adam Washington <adam.washington@stfc.ac.uk>
2017-09-14 10:24:05 +0000

    Pull mask from Workspace

--- sans/Larmor/Semsans/Polarisation.py
+++ sans/Larmor/Semsans/Polarisation.py
@@ -10,10 +10,12 @@ class Polarisation(PythonAlgorithm):
 
     def PyInit(self):
         self.declareProperty("RunNumber", defaultValue=0)
+        self.declareProperty("Binning", defaultValue="1.8,0.1,8.0")
         self.declareProperty(
-            FileProperty(
-                name="MaskFile", action=FileAction.Load,
-                defaultValue=r'\\isis\inst$\NDXLARMOR\User\Users\Edler\May_2017\Mask_Tube39.xml'))
+            WorkspaceProperty(
+                name="MaskFile",
+                direction=Direction.Input,
+                defaultValue=r'MaskWorkspace'))
         self.declareProperty(
             WorkspaceProperty(name="OutputWorkspace",
                               defaultValue="",
@@ -24,12 +26,13 @@ class Polarisation(PythonAlgorithm):
         w1mon = ExtractSingleSpectrum("w1_monitors", 0)
         w1 = ConvertUnits("w1", "Wavelength", AlignBins=1)
         w1mon = ConvertUnits(w1mon, "Wavelength")
-        w1 = Rebin(w1, "1.8,0.1,8", PreserveEvents=False)
-        w1mon = Rebin(w1mon, "1.8,0.1,8")
+        
+        binning = self.getProperty("Binning").value
+        w1 = Rebin(w1, binning, PreserveEvents=False)
+        w1mon = Rebin(w1mon, binning)
         w1 = w1 / w1mon
 
-        mask = self.getProperty("MaskFile").value
-        Mask_Tube = LoadMask("LARMOR", mask)
+        Mask_Tube = self.getProperty("MaskFile").value
         MaskDetectors(w1, MaskedWorkspace=Mask_Tube)
         w1 = SumSpectra(w1)
 
@@ -37,7 +40,7 @@ class Polarisation(PythonAlgorithm):
         dn = mtd["w1_2"]
         pol = (up-dn)/(up+dn)
         self.setProperty("OutputWorkspace", pol)
-        DeleteWorkspaces([pol, "Mask_Tube", "w1_1", "w1_2", "w1_monitors_1",
+        DeleteWorkspaces([pol, "w1_1", "w1_2", "w1_monitors_1",
                           "w1_monitors_2", "w1mon_1", "w1mon_2"])
 
 AlgorithmFactory.subscribe(Polarisation)
