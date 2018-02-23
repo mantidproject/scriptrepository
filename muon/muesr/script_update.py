a0152aa6988e32245134000e0ef6fd4f09c84715
Claire Wilde <claire.wilde@stfc.ac.uk>
2018-02-23 11:38:41 +0000

    update for muesr script

---
+++ muon/muesr/script_update.py
@@ -0,0 +1,6 @@
+import subprocess
+# install phase
+print(subprocess.Popen("""python -m pip install --upgrade
+pip""",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE).communicate())
+print(subprocess.Popen("""python -m pip install --upgrade --user
+https://github.com/bonfus/muesr/archive/newcif.zip""",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE).communicate())
