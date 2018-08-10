4291cc588fdd6d83fff1fd01df653d931f1643f8
James Lord <james.lord@stfc.ac.uk>
2018-08-10 13:23:31 +0000

    v104

---
+++ muon/Quantum/Examples/standalone.py
@@ -0,0 +1,16 @@
+# try Quantum without Mantid
+from quantumtabletools import RunModelledSystem
+pars={
+	"spins":("Mu","e"),
+	"a(Mu)":(4463,),
+	"measure":("integral",),
+	"loop0par":"bmag",
+	"loop0range":(0.0,1.0,5),
+	}
+q=RunModelledSystem(pars)
+
+x=q[0][0]
+y=q[1]
+e=q[2]
+for i in range(len(x)):
+	print x[i],y[0,i]
