bb6611ae0bb57bdd777ba1a855bb971e05dcd051
Anthony Lim <anthony.lim@stfc.ac.uk>
2019-05-13 12:49:02 +0000

    Install script for QENS lib

---
+++ indirect_inelastic/models/QENS_lib/QENS_lib_install.py
@@ -0,0 +1,13 @@
+#run this in Mantid scripting window
+import subprocess
+
+"""
+You will need to download/checkout the QENS library: https://github.com/QENSlibrary/QENSmodels
+The path below needs to be the full path to where you have saved the above.
+Run this script and the QENSmodels should be available as part of Mantid
+"""
+path_to_QENS_lib = "C:\Users\BTR75544\work\QENSlib\QENSmodels"
+
+# install phase
+print(subprocess.Popen("python -m pip install -U --no-deps "+path_to_QENS_lib ,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE).communicate())
+
