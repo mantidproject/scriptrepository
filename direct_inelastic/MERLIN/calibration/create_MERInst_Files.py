6d6d45c14c3fd5826756e9179cb8b7f105395558
Richard Waite <richard.waite@stfc.ac.uk>
2019-12-11 10:05:43 +0000

    Removed 0o in date of MER script and fixed typo in Nick Draper script
    Changes requested in PR by Martyn

--- direct_inelastic/MERLIN/calibration/create_MERInst_Files.py
+++ direct_inelastic/MERLIN/calibration/create_MERInst_Files.py
@@ -249,8 +249,8 @@ if __name__ == "__main__":
     # The script stores all its results in Manit Default save directory and 
     
      # list of the known cycles with approximate  end dates:          
-    cycle_end_list = {date(2019,0o3,29):'184',date(2019,0o7,19):'191',date(2019,10,25):'192',\
-    date(2019,12,20):'193',date(2999,0o1,0o1):'future'}
+    cycle_end_list = {date(2019,3,29):'184',date(2019,7,19):'191',date(2019,10,25):'192',\
+    date(2019,12,20):'193',date(2999,1,1):'future'}
     
     # old one2one map file used as source for one2one map file used in current cycle
     one2one_source_map = 'one2one_182.map' 
