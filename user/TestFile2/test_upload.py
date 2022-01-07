8871bb743d12cf65d3488e3ea30e65e780d910e4
DavidFair <DavidFair@users.noreply.github.com>
2022-01-07 11:01:00 +0000

    Remove file from testing

--- user/TestFile2/test_upload.py
+++
@@ -1,7 +0,0 @@
-"""Intended to be used for testing upload mechanism
-
-This is a full summary of the module
-"""
-from __future__ import print_function
-print("Hello, World!!! Danny")
-print("Hell tom!!")
