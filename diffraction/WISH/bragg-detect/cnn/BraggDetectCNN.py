1190d6653323118d81e92248562c1e184dcd0e66
warunawickramasingha <waruna.wickramasingha@stfc.ac.uk>
2025-10-28 15:56:25 +0000

    yolov5 inferencing wip - to be tested

--- diffraction/WISH/bragg-detect/cnn/BraggDetectCNN.py
+++ diffraction/WISH/bragg-detect/cnn/BraggDetectCNN.py
@@ -12,71 +12,19 @@ from enum import Enum
 from sklearn.cluster import HDBSCAN
 from sklearn.metrics import silhouette_score
 
+from abc import ABC, abstractmethod
+
+
 class Clustering(Enum):
     QLab = 1
     HDBSCAN = 2
 
 
-class BraggDetectCNN:
-    """
-    Detects Bragg's peaks from a workspace using a pre trained deep learning model created using Faster R-CNN model with a ResNet-50-FPN backbone.
-    Example usage is as shown below.
-
-    #1) Set the path of the .pt file that contains the weights of the pre trained FasterRCNN model
-    cnn_weights_path = r""
-
-    # 2) Create a peaks workspace containing bragg peaks detected with a confidence greater than conf_threshold
-    cnn_bragg_peaks_detector = BraggDetectCNN(model_weights_path=cnn_weights_path, batch_size=64, workers=0, iou_threshold=0.001)
-    cnn_bragg_peaks_detector.find_bragg_peaks(workspace="WISH00042730", conf_threshold=0.0, clustering="QLab", q_tol=0.05)
-    """
-
-    def __init__(self, model_weights_path, batch_size=64, workers=0, iou_threshold=0.001):
-        """
-        :param model_weights_path: Path to the .pt file containing the weights of the pre trained CNN model
-        :param batch_size: Batch size to be used when loading tube data for inferencing. 
-        :param workers: Number of loader worker processes to do multi-process data loading. workers=0 means data loading in main process
-        :param iou_threshold: IOU(Intersection Over Union) threshold to filter out overlapping bounding boxes for detected peaks
-        """
-        self.model_weights_path = model_weights_path
-        self.device = self._select_device()
-        self.model = self._load_cnn_model_from_weights(self.model_weights_path)
-        self.batch_size = batch_size
-        self.workers = workers
-        self.iou_threshold = iou_threshold
-
-
+class BraggDetectDL(ABC):
+    
+    @abstractmethod
     def find_bragg_peaks(self, workspace, output_ws_name="CNN_Peaks", conf_threshold=0.0, clustering=Clustering.QLab.name, **kwargs):
-        """
-        Find bragg peaks using the pre trained FasterRCNN model and create a peaks workspace
-        :param workspace: Workspace name or the object of Workspace from WISH, ex: "WISH0042730"
-        :param output_ws_name: Name of the peaks workspace
-        :param conf_threshold: Confidence threshold to filter peaks inferred from RCNN
-        :param clustering: name of clustering method(QLab or HDBSCAN). Default is QLab
-        :param kwargs: variable keyword params for clustering methods
-        """
-        start_time = time.time()
-        data_set, predicted_indices = self._do_cnn_inferencing(workspace)
-
-        filtered_indices = predicted_indices[predicted_indices[:, -1] > conf_threshold]
-        
-        #Do Clustering
-        print(f"Starting peak clustering with {clustering} method..")
-        clustered_peaks = self._do_peak_clustering(filtered_indices, clustering, **kwargs)
-        cluster_indices_rounded = np.round(clustered_peaks[:, :3]).astype(int)
-        peaksws = createPeaksWorkspaceFromIndices(data_set.get_workspace(), output_ws_name, cluster_indices_rounded, data_set.get_ws_as_3d_array())
-        for ipk, pk in enumerate(peaksws):
-            pk.setIntensity(clustered_peaks[ipk, -1])
-
-        if clustering == Clustering.QLab.name:
-            #Filter peaks by qlab
-            clustering_params = {"q_tol": 0.05 }
-            clustering_params.update(kwargs)
-            BaseSX.remove_duplicate_peaks_by_qlab(peaksws, **clustering_params)
-        
-        print(f"Number of peaks after clustering is = {len(peaksws)}")
-
-        data_set.delete_rebunched_ws()
-        print(f"Bragg peaks finding from FasterRCNN model is completed in {time.time()-start_time:.2f} seconds!")
+        return
 
 
     def _do_peak_clustering(self, detected_peaks, clustering, **kwargs):
@@ -120,6 +68,186 @@ class BraggDetectCNN:
         for peak in selected_peak_indices:
             confidence.append(peakdata[np.where((peak_indices == peak).all(axis=1))[0].item(), -1])
         return np.column_stack((selected_peak_indices, confidence))
+
+
+    def _apply_nms(self, orig_prediction, iou_thresh):
+        keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
+        final_prediction = orig_prediction
+        final_prediction['boxes'] = final_prediction['boxes'][keep]
+        final_prediction['scores'] = final_prediction['scores'][keep]
+        final_prediction['labels'] = final_prediction['labels'][keep]
+        return final_prediction
+
+    def _select_device(self):
+        if tc.cuda.is_available():
+            print("GPU device is found!")
+            return tc.device("cuda")
+        else:
+            warnings.warn(
+                "Warning! GPU is not available, the program will run very slow..", RuntimeWarning)
+            return tc.device("cpu")
+
+
+    def cluster_detected_peaks(self, clustering, filtered_indices, data_set, output_ws_name, **kwargs):
+        #Do Clustering
+        print(f"Starting peak clustering with {clustering} method..")
+        clustered_peaks = self._do_peak_clustering(filtered_indices, clustering, **kwargs)
+        cluster_indices_rounded = np.round(clustered_peaks[:, :3]).astype(int)
+        peaksws = createPeaksWorkspaceFromIndices(data_set.get_workspace(), output_ws_name, cluster_indices_rounded, data_set.get_ws_as_3d_array())
+        for ipk, pk in enumerate(peaksws):
+            pk.setIntensity(clustered_peaks[ipk, -1])
+
+        if clustering == Clustering.QLab.name:
+            #Filter peaks by qlab
+            clustering_params = {"q_tol": 0.05 }
+            clustering_params.update(kwargs)
+            BaseSX.remove_duplicate_peaks_by_qlab(peaksws, **clustering_params)
+        
+        print(f"Number of peaks after clustering is = {len(peaksws)}")
+
+
+
+class BraggDetectYOLO(BraggDetectDL):
+    def __init__(self, model_weight_path):
+        warnings.simplefilter(action='ignore', category=FutureWarning)
+
+        self.model_weights_path = model_weight_path
+        self.model = tc.hub.load('untralytics/yolov5', 'custom', path=self.model_weights_path)
+        
+
+    def find_bragg_peaks(self, workspace, output_ws_name="YOLO_Peaks", conf_threshold=0.0, clustering=Clustering.QLab.name, **kwargs):
+        print("BraggDetectYOLO: find_bragg_peaks")
+
+        start_time = time.time()
+        data_set = WISHWorkspaceDataSet(workspace)
+        predicted_indices = self._do_yolo_inferencing(data_set)
+        filtered_indices = predicted_indices[predicted_indices[:, -1] > conf_threshold]
+        
+        self.cluster_detected_peaks(clustering, filtered_indices, data_set, output_ws_name, **kwargs)
+        data_set.delete_rebunched_ws()
+        print(f"Bragg peaks finding from FasterRCNN model is completed in {time.time()-start_time:.2f} seconds!")
+
+    
+    def _do_yolo_inferencing(self, data_set):
+        print("BraggDetectYOLO: _do_yolo_inferencing")
+
+        ws_3d_arr = data_set.get_ws_as_3d_array()
+        print(ws_3d_arr.shape)
+        start_t = time.time()
+        yolo_results = []
+        for tubeData in tqdm(ws_3d_arr[::, ...]):
+            tubeDataNorm = ((tubeData - tubeData.min()) / (tubeData.max() - tubeData.min()) * 255).astype(np.uint8)
+            yolo_results.append(self.model(tubeDataNorm))
+
+        print(f"Time to complete yolo inferencing = {time.time() - start_t} seconds")
+
+        predicted_indices_with_score = []
+        for tube_idx, res in enumerate(yolo_results):
+            if len(res.xyxy[0] > 0):
+                print(f"tube {tube_idx} has got peaks..")
+                boxes = res.xyxy[0].cpu().numpy()
+                for box in boxes:
+                    print(f"{box}")
+                    tof = (box[0] + box[2])/2
+                    tube_res = (box[1] + box[3])/2
+                    conf_score = box[4]
+
+                    print(f"At box sum, the shape={ws_3d_arr.shape}")
+                    boxsum = np.sum(ws_3d_arr[tube_idx, box[1]:box[3], box[0]:box[2]])
+                    predicted_indices_with_score.append([tube_idx, tube_res, tof, boxsum, conf_score.cpu()])
+        
+        return np.array(predicted_indices_with_score)
+
+
+class BraggDetectCNN(BraggDetectDL):
+    """
+    Detects Bragg's peaks from a workspace using a pre trained deep learning model created using Faster R-CNN model with a ResNet-50-FPN backbone.
+    Example usage is as shown below.
+
+    #1) Set the path of the .pt file that contains the weights of the pre trained FasterRCNN model
+    cnn_weights_path = r""
+
+    # 2) Create a peaks workspace containing bragg peaks detected with a confidence greater than conf_threshold
+    cnn_bragg_peaks_detector = BraggDetectCNN(model_weights_path=cnn_weights_path, batch_size=64, workers=0, iou_threshold=0.001)
+    cnn_bragg_peaks_detector.find_bragg_peaks(workspace="WISH00042730", conf_threshold=0.0, clustering="QLab", q_tol=0.05)
+    """
+
+    def __init__(self, model_weights_path, batch_size=64, workers=0, iou_threshold=0.001):
+        """
+        :param model_weights_path: Path to the .pt file containing the weights of the pre trained CNN model
+        :param batch_size: Batch size to be used when loading tube data for inferencing. 
+        :param workers: Number of loader worker processes to do multi-process data loading. workers=0 means data loading in main process
+        :param iou_threshold: IOU(Intersection Over Union) threshold to filter out overlapping bounding boxes for detected peaks
+        """
+        self.model_weights_path = model_weights_path
+        self.device = self._select_device()
+        self.model = self._load_cnn_model_from_weights(self.model_weights_path)
+        self.batch_size = batch_size
+        self.workers = workers
+        self.iou_threshold = iou_threshold
+
+
+    def find_bragg_peaks(self, workspace, output_ws_name="CNN_Peaks", conf_threshold=0.0, clustering=Clustering.QLab.name, **kwargs):
+        """
+        Find bragg peaks using the pre trained FasterRCNN model and create a peaks workspace
+        :param workspace: Workspace name or the object of Workspace from WISH, ex: "WISH0042730"
+        :param output_ws_name: Name of the peaks workspace
+        :param conf_threshold: Confidence threshold to filter peaks inferred from RCNN
+        :param clustering: name of clustering method(QLab or HDBSCAN). Default is QLab
+        :param kwargs: variable keyword params for clustering methods
+        """
+        start_time = time.time()
+        data_set, predicted_indices = self._do_cnn_inferencing(workspace)
+
+        filtered_indices = predicted_indices[predicted_indices[:, -1] > conf_threshold]
+        
+        self.cluster_detected_peaks(clustering, filtered_indices, data_set, output_ws_name, **kwargs)
+
+        data_set.delete_rebunched_ws()
+        print(f"Bragg peaks finding from FasterRCNN model is completed in {time.time()-start_time:.2f} seconds!")
+
+
+    # def _do_peak_clustering(self, detected_peaks, clustering, **kwargs):
+    #     print(f"Number of peaks before clustering = {len(detected_peaks)}")
+    #     if clustering == Clustering.HDBSCAN.name:
+    #         return self._do_hdbscan_clustering(detected_peaks, **kwargs)
+    #     else:
+    #         return detected_peaks
+
+
+    # def _do_hdbscan_clustering(self, peakdata, keep_ignored_labels=True, **kwargs):
+    #     """
+    #     Do HDBSCAN clustering over the inferred peak coordinates
+    #     :param peakata: np array containig the inferred peak coordinates
+    #     :param keep_ignored_labels: whether to include the unclustered peaks in final result.
+    #         default is True, can be set to False via passing "keep_ignored_labels": False in kwargs
+    #     :param kwargs: variable keyword params to be passed to HDBSCAN algorithm 
+    #         https://scikit-learn.org/1.5/modules/generated/sklearn.cluster.HDBSCAN.html
+    #     """
+    #     peak_indices = np.delete(peakdata, [3,4], axis=1)
+    #     if ("keep_ignored_labels" in kwargs):
+    #         keep_ignored_labels = kwargs.pop("keep_ignored_labels")
+
+    #     hdbscan_params = {"min_cluster_size": 2, 
+    #                       "min_samples": 2, 
+    #                       "store_centers" : "medoid", 
+    #                       "algorithm": "auto", 
+    #                       "cluster_selection_method": "eom", 
+    #                       "metric": "euclidean"
+    #                       }
+    #     hdbscan_params.update(kwargs)
+    #     hdbscan = HDBSCAN(**hdbscan_params)
+    #     hdbscan.fit(peak_indices)
+    #     print(f"Silhouette score of the clusters={silhouette_score(peak_indices, hdbscan.labels_)}")
+
+    #     if keep_ignored_labels:
+    #         selected_peak_indices = np.concatenate((hdbscan.medoids_, peak_indices[np.where(hdbscan.labels_==-1)]), axis=0)
+    #     else:
+    #         selected_peak_indices = hdbscan.medoids_
+    #     confidence = []
+    #     for peak in selected_peak_indices:
+    #         confidence.append(peakdata[np.where((peak_indices == peak).all(axis=1))[0].item(), -1])
+    #     return np.column_stack((selected_peak_indices, confidence))
     
 
     def _do_cnn_inferencing(self, workspace):
@@ -145,23 +273,23 @@ class BraggDetectCNN:
         return data_set, np.array(predicted_indices_with_score)
                     
 
-    def _apply_nms(self, orig_prediction, iou_thresh):
-        keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
-        final_prediction = orig_prediction
-        final_prediction['boxes'] = final_prediction['boxes'][keep]
-        final_prediction['scores'] = final_prediction['scores'][keep]
-        final_prediction['labels'] = final_prediction['labels'][keep]
-        return final_prediction
+    # def _apply_nms(self, orig_prediction, iou_thresh):
+    #     keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
+    #     final_prediction = orig_prediction
+    #     final_prediction['boxes'] = final_prediction['boxes'][keep]
+    #     final_prediction['scores'] = final_prediction['scores'][keep]
+    #     final_prediction['labels'] = final_prediction['labels'][keep]
+    #     return final_prediction
 
 
-    def _select_device(self):
-        if tc.cuda.is_available():
-            print("GPU device is found!")
-            return tc.device("cuda")
-        else:
-            warnings.warn(
-                "Warning! GPU is not available, the program will run very slow..", RuntimeWarning)
-            return tc.device("cpu")
+    # def _select_device(self):
+    #     if tc.cuda.is_available():
+    #         print("GPU device is found!")
+    #         return tc.device("cuda")
+    #     else:
+    #         warnings.warn(
+    #             "Warning! GPU is not available, the program will run very slow..", RuntimeWarning)
+    #         return tc.device("cpu")
 
 
     def _load_cnn_model_from_weights(self, weights_path):
